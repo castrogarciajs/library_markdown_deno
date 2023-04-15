9192ea64d28bb4e370188ec27eb084cb11043635
Sebastian009w <ia3266595@gmail.com>
2023-04-14 21:15:09 -0500

    se agrega ambas opciones

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { Markdown } from "./cli/markdown.ts";
 import { Path } from "./cli/path.ts";
-import { Status } from './cli/status.ts';
+import { Status } from "./cli/status.ts";
 
 export function markdown_deno(path_name: string, option: string[]) {
   const markdown = new Markdown();
@@ -9,6 +9,11 @@ export function markdown_deno(path_name: string, option: string[]) {
 
   const array = markdown.get_all_markdown(path_name);
 
+  if (option[0] !== "--validate" && option[0] !== "--stats")
+    return console.error(
+      "La Opcion ingresada no se encuentra disponible, si quieres mas informacion utiliza la opcion --help"
+    );
+
   if (option[0] === undefined && option[1] === undefined) {
     array.forEach((item) => {
       const resolve = path.validate_path(item);
@@ -16,21 +21,33 @@ export function markdown_deno(path_name: string, option: string[]) {
     });
   }
 
-  if (option[0] === "--evaluate") {
+  if (option[0] === "--validate") {
     array.forEach((item) => {
       const resolve = path.validate_path(item);
       status
         .validate_link(resolve)
         .then((data) => console.info(data))
-        .catch((err) => console.error(err));
+        .catch((err) => console.error(err.message));
     });
   }
 
-  if(option[0] === '--stats') {
+  if (option[0] === "--stats") {
     array.forEach((item) => {
-      const resolve = path.validate_path(item)    
-      const stats = status.stats_unique(resolve)      
-      console.log(stats)
-    })
+      const resolve = path.validate_path(item);
+      const stats = status.stats_unique(resolve);
+      console.log(stats);
+    });
+  }
+  if (
+    (option[0] === "--stats" && option[1] === "--validate") ||
+    (option[0] === "--validate" && option[1] === "--stats")
+  ) {
+    array.forEach((item) => {
+      const resolve = path.validate_path(item);
+      status
+        .validate_link(resolve)
+        .then((data) => console.info(status.broken_link(data)))
+        .catch((err) => console.error(err.message));
+    });
   }
 }
